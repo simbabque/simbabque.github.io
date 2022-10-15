7d785b8620f3b27fd71ab75274d24d1ca6fe6ab6
simbabque <simbabque@users.noreply.github.com>
2022-10-15 09:30:20 +0000

    Deploying to gh-pages from @ simbabque/simbabque.github.io@436e7d394a3c9624bf424ddc53b2a8358802ff2b 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
 // nothing here yet.
+$('code.language-mermaid').each(function(index, element) {
+    var content = $(element).html().replace(/&amp;/g, '&');
+    $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
+});
